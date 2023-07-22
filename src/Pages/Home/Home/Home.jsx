3c18a007c8494f844312fab2215c9df0606d78e1
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-07-22 12:14:16 +0600

    Navbar and footer added

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+
+
+
+const Home = () => {
+    return (
+        <div className="bg-black text-white">
+
+        </div>
+    );
+};
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+
+    },
+
   },
+
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["dracula"],
+  },
 }
+
 
