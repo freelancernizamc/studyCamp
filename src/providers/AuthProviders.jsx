fa2d6c7dc8e9fb546647678f43ce13a11cd7235c
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-09-11 15:29:45 +0600

    email js implemented

--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import axios from "axios";
 // import axios from "axios";
 
 export const AuthContext = createContext();
